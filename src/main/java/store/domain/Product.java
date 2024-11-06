384faa3b01b01494e79f51b83a1178d8b8e9d5a7
joonwan <joonwan1024@naver.com>
2024-11-07 04:37:49 +0900

    feat: 상품 저장소 구현

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,4 +1,35 @@
 package store.domain;
 
 public class Product {
+
+    private String name;
+    private int price;
+    private int promotionQuantity;
+    private int defaultQuantity;
+    private Promotion promotion;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public boolean hasName(String name) {
+        return this.name.equals(name);
+    }
+
+    public void increasePromotionQuantity(int amount) {
+        this.promotionQuantity += amount;
+    }
+
+    public void increaseDefaultQuantity(int amount) {
+        this.defaultQuantity += amount;
+    }
+
+    public void applyPromotion(Promotion promotion) {
+        this.promotion = promotion;
+    }
+
+    public void endPromotion() {
+        this.promotion = null;
+    }
 }
--- src/main/java/store/repository/ProductRepository.java
+++ src/main/java/store/repository/ProductRepository.java
@@ -1,4 +1,45 @@
 package store.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+import store.domain.Product;
+
 public class ProductRepository {
+
+    private static long id;
+    private final Map<Long, Product> store = new HashMap<>();
+
+    public void save(Product product) {
+        store.put(++id, product);
+    }
+
+    public Product findByName(String name) {
+        validateNotNull(name);
+
+        for (Long id : store.keySet()) {
+            Product product = store.get(id);
+            if (product.hasName(name)) {
+                return product;
+            }
+        }
+        throw new IllegalArgumentException("해당 이름을 가진 상품이 존재하지 않습니다.");
+    }
+
+    public int size() {
+        return store.size();
+    }
+
+    public boolean contains(Product product) {
+        return store.containsValue(product);
+    }
+
+    public void clear() {
+        store.clear();
+    }
+
+    private void validateNotNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름으로 상품을 찾을 때 null 을 전달할 수 없습니다.");
+        }
+    }
 }
