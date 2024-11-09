3512c151684430069cb311b4a01b611c7bd70251
joonwan <joonwan1024@naver.com>
2024-11-09 22:45:52 +0900

    test: 주문 상품 타입 검증 테스트 코드 작성

---
+++ src/main/java/store/domain/OrderProductType.java
@@ -0,0 +1,8 @@
+package store.domain;
+
+
+public enum OrderProductType {
+
+    DEFAULT, ADDITIONAL_PROMOTION, PARTIAL_PROMOTION;
+
+}
--- src/test/java/store/domain/OrderProductTest.java
+++ src/test/java/store/domain/OrderProductTest.java
@@ -1,14 +1,28 @@
 package store.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static store.domain.OrderProductType.*;
 
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import store.dto.OrderProductStatus;
 
 class OrderProductTest {
 
     private Product product = new Product("name", 100, 7, 10);
+    private LocalDateTime startDateTime = LocalDateTime.of(2024, 11, 1, 0, 0, 0);
+    private LocalDateTime endDateTime = LocalDateTime.of(2024, 11, 30, 23, 59, 59);
+    private Promotion promotion = new Promotion("name", 2, 1, startDateTime, endDateTime);
+
+    @BeforeEach
+    void registerPromotion() {
+        product.registerPromotion(promotion);
+    }
 
     @DisplayName("주문 수량은 상품의 재고 수량을 초과할 수 없다.")
     @ParameterizedTest
@@ -18,7 +32,6 @@ class OrderProductTest {
         assertThatThrownBy(() -> new OrderProduct(product, orderQuantity))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("주문 수량은 총 재고수량을 초과할 수 없습니다.");
-
 
     }
 
@@ -33,4 +46,75 @@ class OrderProductTest {
 
     }
 
+    @DisplayName("프로모션이 적용되며 프로모션 재고를 초과하여 일부 재고에 프로모션이 적용되지 않을 경우 주문상품의 타입은 PARTIAL_PROMOTION 이다.")
+    @ParameterizedTest
+    @CsvSource(value = {"8,0,2", "9,0,3", "10,0,4", "11,0,5"})
+    void partialPromotionType(int orderQuantity, int additionalGiftProductCount, int notApplicableProductCount) {
+        OrderProduct orderProduct = new OrderProduct(product, orderQuantity);
+        LocalDateTime promotionApplicableDateTime = LocalDateTime.of(2024, 11, 1, 0, 0, 0);
+
+        OrderProductStatus orderProductStatus = orderProduct.getOrderProductStatus(promotionApplicableDateTime);
+
+        assertThat(orderProductStatus.getOrderProductType()).isEqualTo(PARTIAL_PROMOTION);
+        assertThat(orderProductStatus.getAdditionalGiftProductCount()).isEqualTo(additionalGiftProductCount);
+        assertThat(orderProductStatus.getNotApplicableProductCount()).isEqualTo(notApplicableProductCount);
+    }
+
+
+    @DisplayName("주문 수량을 프로모션 재고 내에서 처리 가능하며 증정이 가능한 경우 주문 상품의 타입은 ADDITIONAL_PRODUCT 이다.")
+    @ParameterizedTest
+    @CsvSource(value = {"5,1,0", "2,1,0"})
+    void additionalPromotion(int orderQuantity, int additionalGiftProductCount, int notApplicableProductCount) {
+        OrderProduct orderProduct = new OrderProduct(product, orderQuantity);
+        LocalDateTime promotionApplicableDateTime = LocalDateTime.of(2024, 11, 1, 0, 0, 0);
+
+        OrderProductStatus orderProductStatus = orderProduct.getOrderProductStatus(promotionApplicableDateTime);
+
+        assertThat(orderProductStatus.getOrderProductType()).isEqualTo(ADDITIONAL_PROMOTION);
+        assertThat(orderProductStatus.getAdditionalGiftProductCount()).isEqualTo(additionalGiftProductCount);
+        assertThat(orderProductStatus.getNotApplicableProductCount()).isEqualTo(notApplicableProductCount);
+    }
+
+    @DisplayName("프로모션이 존재하지 않는 주문 상품일 경우 해당 주문 상품의 타입은 DEFAULT 이다.")
+    @ParameterizedTest
+    @CsvSource(value = {"10,0,0", "4,0,0"})
+    void notPromotionProduct(int orderQuantity, int additionalGiftProductCount, int notApplicableProductCount) {
+        product.clearPromotion();
+        OrderProduct orderProduct = new OrderProduct(product, orderQuantity);
+        LocalDateTime promotionApplicableDateTime = LocalDateTime.of(2024, 11, 1, 0, 0, 0);
+
+        OrderProductStatus orderProductStatus = orderProduct.getOrderProductStatus(promotionApplicableDateTime);
+
+        assertThat(orderProductStatus.getOrderProductType()).isEqualTo(DEFAULT);
+        assertThat(orderProductStatus.getAdditionalGiftProductCount()).isEqualTo(additionalGiftProductCount);
+        assertThat(orderProductStatus.getNotApplicableProductCount()).isEqualTo(notApplicableProductCount);
+    }
+
+    @DisplayName("프로모션이 적용되지 않는 시간 범위의 주문 상품일 경우 타입은 DEFUALT 이다.")
+    @ParameterizedTest
+    @CsvSource(value = {"10,0,0", "4,0,0"})
+    void notPromotionDateTime(int orderQuantity, int additionalGiftProductCount, int notApplicableProductCount) {
+        OrderProduct orderProduct = new OrderProduct(product, orderQuantity);
+        LocalDateTime promotionApplicableDateTime = LocalDateTime.of(2024, 12, 1, 0, 0, 0);
+
+        OrderProductStatus orderProductStatus = orderProduct.getOrderProductStatus(promotionApplicableDateTime);
+
+        assertThat(orderProductStatus.getOrderProductType()).isEqualTo(DEFAULT);
+        assertThat(orderProductStatus.getAdditionalGiftProductCount()).isEqualTo(additionalGiftProductCount);
+        assertThat(orderProductStatus.getNotApplicableProductCount()).isEqualTo(notApplicableProductCount);
+    }
+
+    @DisplayName("프로모션이 존재하며 프로모션 수량 내에서 주문 수량을 처리가능하지만 추가증정이 더이상 안될경우 타입은 DEFAULT 이다.")
+    @ParameterizedTest
+    @CsvSource(value = {"7,0,0", "6,0,0"})
+    void notEnoughToAdditionalPromotion(int orderQuantity, int additionalGiftProductCount, int notApplicableProductCount) {
+        OrderProduct orderProduct = new OrderProduct(product, orderQuantity);
+        LocalDateTime promotionApplicableDateTime = LocalDateTime.of(2024, 11, 1, 0, 0, 0);
+
+        OrderProductStatus orderProductStatus = orderProduct.getOrderProductStatus(promotionApplicableDateTime);
+
+        assertThat(orderProductStatus.getOrderProductType()).isEqualTo(DEFAULT);
+        assertThat(orderProductStatus.getAdditionalGiftProductCount()).isEqualTo(additionalGiftProductCount);
+        assertThat(orderProductStatus.getNotApplicableProductCount()).isEqualTo(notApplicableProductCount);
+    }
 }
